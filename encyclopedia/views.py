523d854429df2aa71fd93611e9d9fc6deb35596a
David Rodriguez <rodriguez.davidp@outlook.com>
2023-10-02 13:07:56 -0800

    - added wiki template - linked index list to each page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def wiki(request, name):
+    return render(request, "encyclopedia/wiki.html", {
+        "entry": util.get_entry(name)
+    })
